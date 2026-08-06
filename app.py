217ab4ec3aa3668ad15f36b5a815f2c7a8d4f8be
yasht9211 <118149620+yasht9211@users.noreply.github.com>
2026-08-06 20:46:18 +0530

    Modify text-shadow for improved styling
    Updated text-shadow property for better visibility.

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ st.markdown(
     [data-testid="stAppViewContainer"] div[data-testid="stMarkdownContainer"],
     [data-testid="stAppViewContainer"] .stCaption {{
         color: #382c3d !important;
-        text-shadow: 0 0 10px rgba(255,255,255,0.95), 0 0 6px rgba(255,255,255,0.9), 0 0 3px rgba(255,255,255,0.9);
+        text-shadow: 0 0 10px rgba(255,255,255,1), 0 0 6px rgba(255,255,255,0.9), 0 0 3px rgba(255,255,255,0.9);
     }}
     [data-testid="stAppViewContainer"] h1 {{
         font-weight: 800 !important;
